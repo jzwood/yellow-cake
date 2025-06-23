38dc046331949536b221185f8a2ef55e80c70373
Jake Wood <jzwood14@yahoo.com>
2025-06-22 21:54:54 -0400

    setting up playground

--- web/index.js
+++ web/index.js
@@ -1,9 +1,11 @@
-import { run } from "./src/interpreter.js";
+import { run } from "../src/interpreter.js";
 
 function main() {
+  const input = document.getElementById("input");
+  const output = document.getElementById("input");
   // READ PROGRAM FROM TEXTAREA
   // GET GENERATOR
-  const program = run(program);
+  //const program = run(program);
 
   // ADD EVENT HANDLER TO THE STEP FUNCTION
   // ON CLICK CALL program.next() and UPDATE UI
@@ -12,3 +14,5 @@ function main() {
 //const { stack, memory, fuel } = await main();
 //console.log(stack);
 //console.warn({ fuel });
+
+document.addEventListener("DOMContentLoaded", main);
